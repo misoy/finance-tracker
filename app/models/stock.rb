37b5d6d30ad098a7bdc7f83cb7e7bc1bbfa4df04
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-22 17:27:11 +0800

    Create UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     iex = Rails.application.credentials.iex_client
