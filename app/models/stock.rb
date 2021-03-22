2858bd5b5d94a894f7e32c789e8c809457b361a2
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-22 12:34:59 +0800

    Create stock new lookup method and add iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    iex = Rails.application.credentials.iex_client
+    client = IEX::Api::Client.new(publishable_token: iex[:sandbox_publishable_token],
+                                  secret_token: iex[:sandbox_secret_token],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
+
 end
