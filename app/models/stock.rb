55c2bd08d6e4940bacc9f57a3784059beb14e134
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-22 15:17:46 +0800

    Return stock object upon search and add logic for invalid input

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,11 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: iex[:sandbox_publishable_token],
                                   secret_token: iex[:sandbox_secret_token],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 
 end
